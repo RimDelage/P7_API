e9006bd576081b74fc605c6b522e83c414ca173b
RimDelage <Rim_delage@hotmail.com>
2023-06-08 11:17:53 +0200

    test hello !

--- api.py
+++ api.py
@@ -68,7 +68,7 @@ def list_id_clients():
 
 @app.route('/', methods=['GET'])
 def Hello ():
-    return 'hello world !!!!!'
+    return 'hello world !!!!!!!'
 
 
 # @app.route('/predict/<id_client>', methods=['GET'])
